d90a475fc8fc40659229e1b015c99e314a3a4010
João Artur <jfgartur@hotmail.com>
2022-12-11 14:27:34 +0000

    Complete requirements

--- SI_Kafka_Classes/kakfa/src/main/java/com/example/KakfaStreams.java
+++ SI_Kafka_Classes/kakfa/src/main/java/com/example/KakfaStreams.java
@@ -12,6 +12,8 @@ import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.Grouped;
+import org.apache.kafka.streams.kstream.JoinWindows;
+import org.apache.kafka.streams.kstream.Joined;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
 import org.apache.kafka.streams.kstream.Materialized;
@@ -35,28 +37,22 @@ public class KakfaStreams {
 
     private void myMain() {
 
+        //
         Properties standardProps = loadProperties(1);
-        // Properties alertProps = loadProperties(2);
 
         StreamsBuilder builder = new StreamsBuilder();
-        KStream<String, String> lines = builder.stream(STANDARD_WEATHER_TOPIC);
-        KStream<String, String> alerts = builder.stream(WEATHER_ALERTS_TOPIC);
-
-        // // Window
-        // Duration windowSize = Duration.ofMinutes(5);
-        // Duration advanceSize = Duration.ofMinutes(1);
-        // TimeWindows hoppingWindow = TimeWindows.ofSizeWithNoGrace(windowSize).advanceBy(advanceSize);
+        KStream<String, String> standardEvents = builder.stream(STANDARD_WEATHER_TOPIC);
+        KStream<String, String> alertEvents = builder.stream(WEATHER_ALERTS_TOPIC);
         
-
         // 1. Count temperature readings of standard weather events per weather station.
-        KTable<String, Long> outlines = lines
+        KTable<String, Long> outlines = standardEvents
         .groupByKey()
         .count();
 
         outlines
         .mapValues((k,v) -> {
-            String result = "{\"station\": " + k + ", \"readings\": " + v + "}";
-            writeToFile("1.txt", result);
+            //String result = "{\"station\": " + k + ", \"readings\": " + v + "}";
+            //writeToFile("1.txt", result);
             return jsonToBD("station", k, "readings", String.valueOf(v), false);
         })
         .toStream()
@@ -64,15 +60,15 @@ public class KakfaStreams {
 
 
         // 2. Count temperature readings of standard weather events per location.
-        KTable<String, Long> readingPerLocation = lines
+        KTable<String, Long> readingPerLocation = standardEvents
         .selectKey((key, value) -> loadJSONAttribute(value, "location"))
         .groupByKey()
         .count();
 
         readingPerLocation
         .mapValues((key, value) -> {
-            String result = "{\"location\": " + key + ", \"readings\": " + value + "}";
-            writeToFile("2.txt", result);
+            //String result = "{\"location\": " + key + ", \"readings\": " + value + "}";
+            //writeToFile("2.txt", result);
             return jsonToBD("location", key, "readings", String.valueOf(value), false);
         })
         .toStream()
@@ -80,7 +76,7 @@ public class KakfaStreams {
 
 
         // 3. Get minimum and maximum temperature per weather station.
-        KTable<String, int[]> minMaxTempPerStation = lines
+        KTable<String, int[]> minMaxTempPerStation = standardEvents
         .groupByKey()
         .aggregate(() -> new int[]{Integer.MAX_VALUE, Integer.MIN_VALUE}, (aggKey, newValue, aggValue) -> {
             int temperature = loadJSONIntAttribute(newValue, "temperature");
@@ -92,16 +88,17 @@ public class KakfaStreams {
         }, Materialized.with(Serdes.String(), new IntArraySerde()));
 
         minMaxTempPerStation.mapValues((key, values) -> {
-            String result = "{\"station\": " + key + ", \"minTemperature\": " + values[0]  + ", \"maxTemperature\":" + values[1] + "}";
-            writeToFile("3.txt", result);
-            return jsonToBDMultipleColumns("station", key, "minTemperature", values[0], "maxTemperature", values[1]);
+            //String result = "{\"station\": " + key + ", \"minTemperature\": " + values[0]  + ", \"maxTemperature\":" + values[1] + "}";
+            //writeToFile("3.txt", result);
+            return jsonToBDMultipleColumns("station", key, "minTemperature",
+             values[0], "maxTemperature", values[1]);
         })
         .toStream()
         .to(OUTPUT_TOPIC + "-3", Produced.with(Serdes.String(), Serdes.String()));
 
 
         // 4. Get minimum and maximum temperature per location (Students should compute these values in Fahrenheit).
-        KTable<String, int[]> minMaxPerLocation = lines
+        KTable<String, int[]> minMaxPerLocation = standardEvents
         .selectKey((key, value) -> loadJSONAttribute(value, "location"))
         .groupByKey()
         .aggregate(() -> new int[]{Integer.MAX_VALUE, Integer.MIN_VALUE}, (aggKey, newValue, aggValue) -> {
@@ -115,34 +112,36 @@ public class KakfaStreams {
 
 
         minMaxPerLocation.mapValues((key, values) -> {
-            String result = "{\"location\": " + key + ", \"minTemperature\": " + celsiusToFahrenheit(values[0]) + ", \"maxTemperature\":" + celsiusToFahrenheit(values[1]) + "}";
-            writeToFile("4.txt", result);
-            return jsonToBDMultipleColumns("location", key, "minTemperature", values[0], "maxTemperature", values[1]);
+            //String result = "{\"location\": " + key + ", \"minTemperature\": " + celsiusToFahrenheit(values[0]) + ", \"maxTemperature\":" + celsiusToFahrenheit(values[1]) + "}";
+            //writeToFile("4.txt", result);
+            return jsonToBDMultipleColumns("location", key, "minTemperature",
+             values[0], "maxTemperature", values[1]);
         })
         .toStream()
         .to(OUTPUT_TOPIC + "-4", Produced.with(Serdes.String(), Serdes.String()));
 
 
         // 5. Count the total number of alerts per weather station.
-        alerts
+        alertEvents
         .groupByKey()
         .count()
         .mapValues((k,v) -> {
-            String result = "{\"station\": " + k + ", \"alerts\": " + v + "}";
-            writeToFile("5.txt", result);
+            //String result = "{\"station\": " + k + ", \"alerts\": " + v + "}";
+            //writeToFile("5.txt", result);
             return jsonToBD("station", k, "alerts", String.valueOf(v), false);
         })
         .toStream()
         .to(OUTPUT_TOPIC + "-5", Produced.with(Serdes.String(), Serdes.String()));
 
+
         // 6. Count the total alerts per type
-        alerts
+        alertEvents
         .selectKey((key, value) -> loadJSONAttribute(value, "type"))
         .groupByKey()
         .count()
         .mapValues((key, value) -> {
-            String result = "{\"alertType\": " + key + ", \"readings\": " + value + "}";
-            writeToFile("6.txt", result);
+            //String result = "{\"alertType\": " + key + ", \"readings\": " + value + "}";
+            //writeToFile("6.txt", result);
             return jsonToBD("alertType", key, "readings", String.valueOf(value), false);
         })
         .toStream()
@@ -152,7 +151,7 @@ public class KakfaStreams {
         // 7. Get minimum temperature of weather stations with red alert events.
 
         // Get weather stations in red zones
-        KStream<String, String> weatherStationsWithRedAlerts = alerts.filter((key, value) -> {
+        KStream<String, String> weatherStationsWithRedAlerts = alertEvents.filter((key, value) -> {
             return loadJSONAttribute(value, "type").compareTo("red") == 0;
         });
 
@@ -163,61 +162,85 @@ public class KakfaStreams {
 
         minTempRedStream
         .groupByKey()
-        .aggregate(() -> 0, (aggKey, newValue, aggValue) -> {
-            return newValue;
-        }, Materialized.with(Serdes.String(), new IntegerSerde()))
+        .reduce((oldValue, newValue) -> newValue, Materialized.with(Serdes.String(), new IntegerSerde()))
         .mapValues((key, value) -> {
-            String result = "{\"redAlertStation\": " + key + ", \"minTemperature\": " + value + "}";
-            writeToFile("7.txt", result);
+            //String result = "{\"redAlertStation\": " + key + ", \"minTemperature\": " + value + "}";
+            //writeToFile("7.txt", result);
             return jsonToBD("redAlertStation", key, "minTemperature", String.valueOf(value), false);
         })
         .toStream()
         .to(OUTPUT_TOPIC + "-7", Produced.with(Serdes.String(), Serdes.String()));
 
-
         // 8. Get maximum temperature of each location of alert events for the last hour
 
-        // Get locations with red alerts
-        KStream<String, String> locationsWithRedAlerts = alerts
-        .selectKey((key, value) -> loadJSONAttribute(value, "location"))
-        .filter((key, value) -> {
-            return loadJSONAttribute(value, "type").compareTo("red") == 0;
-        });
+        // Get locations with alerts
+        KStream<String, String> locationsWithAlerts = alertEvents
+        .selectKey((key, value) -> loadJSONAttribute(value, "location"));
 
-        KStream<String, Integer> maxTempRedLocation = locationsWithRedAlerts.join(minMaxPerLocation,
-            (left, right) -> right[1]
+        // Get temperature reading of each location
+        KStream<String, String> locationTemperatures = standardEvents
+        .selectKey((key, value) -> loadJSONAttribute(value, "location"));
+
+        // Get temperature reading of each alert location
+        KStream<String, Integer> alertLocationTemperatures = locationsWithAlerts
+        .join(
+            locationTemperatures, 
+            (left, right) -> loadJSONIntAttribute(right, "temperature"),
+             JoinWindows.ofTimeDifferenceWithNoGrace(Duration.ofMinutes(5))
         );
 
         Duration windowSize = Duration.ofMinutes(5);
         Duration gracePeriod = Duration.ofMinutes(1);
-        TimeWindows tumbling = TimeWindows.ofSizeAndGrace(windowSize, gracePeriod);
+        TimeWindows tumblingWindow = TimeWindows.ofSizeAndGrace(windowSize, gracePeriod);
 
-        maxTempRedLocation
+        alertLocationTemperatures
         .groupByKey(Grouped.with(Serdes.String(), Serdes.Integer()))
-        .windowedBy(tumbling)
-        .reduce((oldValue, newValue) -> newValue)
+        .windowedBy(tumblingWindow)
+        .aggregate(() -> new int[]{Integer.MIN_VALUE}, (aggKey, newValue, aggValue) -> {
+            aggValue[0] = Math.max(aggValue[0], newValue);
+            return aggValue;
+        }, Materialized.with(Serdes.String(), new IntArraySerde()))
         .toStream((wk, v) -> wk.key())
         .mapValues((key, value) -> {
-            String result = "{\"location\": " + key + ", \"maxTemperature\": " + value + "}";
-            writeToFile("8.txt", result);
-            return jsonToBD("location", key, "maxTemperature", String.valueOf(value), false);
+            //String result = "{\"location\": " + key + ", \"maxTemperature\": " + value[0] + "}";
+            //writeToFile("8.txt", result);
+            return jsonToBD("location", key, "maxTemperature", String.valueOf(value[0]), false);
         })
         .to(OUTPUT_TOPIC + "-8", Produced.with(Serdes.String(), Serdes.String()));
 
+
         // 9. Get minimum temperature per weather station in red alert zones. -> Fix
 
-        // Get temperature readings of each location
-        KStream<String, Integer> locationTemps = lines
-        .selectKey((key, value) -> loadJSONAttribute(value, "location"))
-        .mapValues((key, value) -> loadJSONIntAttribute(value, "temperature"));
+        // Join alerts and temperature readings of each weather station
+        KStream<String, String> stationAlertsAndTemperatures = alertEvents
+        .join(
+            standardEvents, 
+            (left, right) -> (convertToJson("type", loadJSONAttribute(left, "type"), "temperature", loadJSONIntAttribute(right, "temperature"))),
+             JoinWindows.ofTimeDifferenceWithNoGrace(Duration.ofMinutes(5))
+        );
 
-        // Get alerts of each location
-        KStream<String, String> locationAlerts = alerts
-        .selectKey((key, value) -> loadJSONAttribute(value, "location"))
-        .mapValues((key, value) -> loadJSONAttribute(value, "type"));
+        // Get weather stations in red zones
+        KStream<String, String> stationTemperaturesInRed = stationAlertsAndTemperatures.filter((key, value) -> {
+            return loadJSONAttribute(value, "type").compareTo("red") == 0;
+        });
+        
+        stationTemperaturesInRed
+        .groupByKey(Grouped.with(Serdes.String(), Serdes.String()))
+        .aggregate(() -> new int[]{Integer.MAX_VALUE}, (aggKey, newValue, aggValue) -> {
+            int temperature = loadJSONIntAttribute(newValue, "temperature");
+            aggValue[0] = Math.min(aggValue[0], temperature);
+            return aggValue;
+        }, Materialized.with(Serdes.String(), new IntArraySerde()))
+        .mapValues((key, value) -> {
+            //String result = "{\"location\": " + key + ", \"maxTemperature\": " + value[0] + "}";
+            //writeToFile("8.txt", result);
+            return jsonToBD("weatherStation", key, "minTemperature", String.valueOf(value[0]), false);
+        })
+        .toStream()
+        .to(OUTPUT_TOPIC + "-9", Produced.with(Serdes.String(), Serdes.String()));
 
         // 10. Get the average temperature per weather stations
-        KTable<String, int[]> averageTemps = lines
+        KTable<String, int[]> averageTemps = standardEvents
             .groupByKey()
             .aggregate(() -> new int[]{0, 0}, (aggKey, newValue, aggValue) -> {
 
@@ -238,24 +261,39 @@ public class KakfaStreams {
                     result = "{\"station\": " + key + ", \"avgTemperature\": " + (1.0 * v[1]) / v[0] + "}";
                     valueDB = String.valueOf((1.0 * v[1]) / v[0]);
                 }
-                writeToFile("10.txt", result);
-                return jsonToBD("station", key, "avgTemperature", valueDB, true);
+                return jsonToBD("weatherStation", key, "avgTemperature", valueDB, true);
             })
             .toStream()
             .to(OUTPUT_TOPIC + "-10", Produced.with(Serdes.String(), Serdes.String()));
 
         // 11. Get the average temperature of weather stations with red alert events for the last hour
 
-        KStream<String, Double> avgRedStream = weatherStationsWithRedAlerts.join(averageTemps, (left, right) -> (right[0] != 0 ? (1.0 * right[1]) / right[0] : 0));
+        stationTemperaturesInRed
+        .groupByKey(Grouped.with(Serdes.String(), Serdes.String()))
+        .windowedBy(tumblingWindow)
+        .aggregate(() -> new int[]{0, 0}, (aggKey, newValue, aggValue) -> {
 
-        //avgRedStream.peek((key,value)-> System.out.println("7- Minimum temperature in alert stations " + String.valueOf(value)));
+            aggValue[0] += 1;
+            aggValue[1] += loadJSONIntAttribute(newValue, "temperature");
 
-        
- 
+            return aggValue;
+        }, Materialized.with(Serdes.String(), new IntArraySerde())).toStream((wk, v) -> wk.key())
+        .mapValues((key, value) -> {
+            String result = "";
+            String valueDB = "null";
+
+            if (value[0] == 0) {
+                result = "{\"station\": " + key + ", \"avgTemperature\": null}";
+            } else {
+                result = "{\"station\": " + key + ", \"avgTemperature\": " + (1.0 * value[1]) / value[0] + "}";
+                valueDB = String.valueOf((1.0 * value[1]) / value[0]);
+            }
+            return jsonToBD("weatherStation", key, "avgTemperature", valueDB, true);
+        })
+        .to(OUTPUT_TOPIC + "-11", Produced.with(Serdes.String(), Serdes.String()));
 
         KafkaStreams streams = new KafkaStreams(builder.build(), standardProps);
         streams.start();
-        
     }
 
     private Properties loadProperties(int consumerID) {
@@ -266,6 +304,13 @@ public class KakfaStreams {
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         
         return props;
+    }
+
+    private String convertToJson(String fieldA, String valueA, String fieldB, int valueB) {
+        return "{ " +
+            fieldA + ": " + valueA + "," +
+            fieldB + ": " + valueB 
+            + "}";
     }
 
     private String loadJSONAttribute(String data, String parameter) {
