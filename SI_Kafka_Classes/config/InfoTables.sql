1a38b33657109a452c6c9273ef998e0e1e88c0f3
Filiperss <filiperss@hotmail.com>
2022-12-13 17:29:18 +0000

    added sql schemas

--- SI_Kafka_Classes/config/InfoTables.sql
+++ SI_Kafka_Classes/config/InfoTables.sql
@@ -1,9 +1,11 @@
-CREATE TABLE IF NOT EXISTS "location" (
-  ID serial PRIMARY KEY,
-  name VARCHAR (255)
-);
+CREATE TABLE IF NOT EXISTS "location" (ID serial PRIMARY KEY, "locationName" VARCHAR (255));
+CREATE TABLE IF NOT EXISTS "station" (ID serial PRIMARY KEY, "stationName" VARCHAR (255));
 
-CREATE TABLE IF NOT EXISTS "station" (
-  ID serial PRIMARY KEY,
-  name VARCHAR (255)
-);
+INSERT INTO "location" VALUES (1, 'Alta');
+INSERT INTO "location" VALUES (2, 'Baixa');
+INSERT INTO "location" VALUES (3, 'Polo II');
+INSERT INTO "location" VALUES (4, 'Norton');
+
+INSERT INTO "station" VALUES (1, 'Coimbra');
+INSERT INTO "station" VALUES (2, 'Lisboa');
+INSERT INTO "station" VALUES (3, 'Porto');
